651e65c5d225e538a984846b18dc0875c8ad2b35
jaimergp <jaimergp@users.noreply.github.com>
2025-11-06 19:41:21 +0100

    prevent some false positives for simple resource names

--- scripts/lint-readme-md.py
+++ scripts/lint-readme-md.py
@@ -14,13 +14,15 @@ for path in sys.argv[1:]:
     missing = []
     path = Path(path)
     readme = path.read_text()
+    readme_words = readme.split()
     for resource in resources:
-        if resource not in readme:
+        if f"`{resource}`" not in readme_words:
             missing.append(resource)
     if missing:
         raise ValueError(
-            f"Some resources are not documented: {missing}. "
-            f"Please edit '{path.resolve()}' accordingly."
+            f"Some resources are not documented:\n\n{missing}\n\n"
+            f"Please edit '{path.resolve()}' accordingly. "
+            "Final result must mention each resource in backticks; e.g. `resource-name`."
         )
 
 sys.exit(0)

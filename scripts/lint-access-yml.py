1b4308ef77eef465d3c30a27adad5f25c64d154d
jaimergp <jaimergp@users.noreply.github.com>
2025-11-06 09:04:17 +0100

    Fix pre-commit config

--- scripts/lint-access-yml.py
+++ scripts/lint-access-yml.py
@@ -1,5 +1,7 @@
 import sys
-import yaml
+from ruamel.yaml import YAML
+
+yaml = YAML(typ="safe")
 
 srv = "cirun-openstack"
 gpu = [f"{srv}-gpu-{x}" for x in ["large", "xlarge", "2xlarge", "4xlarge"]]
@@ -9,8 +11,7 @@ expected_resources = gpu + cpu + win + ["cirun-macos-m4-large"]
 
 for path in sys.argv[1:]:
     with open(path, "r") as f:
-        content = f.read(f)
-    access_yaml = yaml.load(content, Loader=yaml.loader.BaseLoader)
+        access_yaml = yaml.load(f)
     # check resources (and their order) match expectations
     assert [x["resource"] for x in access_yaml["access_control"]] == expected_resources
     # check policies within a resource are ordered

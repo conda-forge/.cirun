e43623b09a42f57523b96430f3896cb2909402bd
H. Vetinari <h.vetinari@gmx.com>
2026-02-24 19:35:47 +1100

    add 7z to windows images

--- runner-images/packer/scripts/build/Install-BasePackages.ps1
+++ runner-images/packer/scripts/build/Install-BasePackages.ps1
@@ -1,9 +1,9 @@
 ################################################################################
 ##  File: Install-BasePackages.ps1
-##  Desc: Install base packages (git, pwsh, ...) via Chocolatey
+##  Desc: Install base packages (git, pwsh, 7z, ...) via Chocolatey
 ################################################################################
 
-Write-Host "Installing Base Packages (git, pwsh, ...)"
+Write-Host "Installing Base Packages (git, pwsh, 7z, ...)"
 
 # for installation options see https://community.chocolatey.org/packages/git, resp.
 # https://github.com/chocolatey-community/chocolatey-packages/blob/master/automatic/git.install/ARGUMENTS.md
@@ -12,6 +12,9 @@ choco install git -y --no-progress --params "'/GitAndUnixToolsOnPath'"
 # necessary to use `shell: pwsh` in GHA
 choco install pwsh -y --no-progress
 
+# necessary to package artefacts
+choco install 7zip -y --no-progress
+
 # Refresh PATH so binaries are reachable immediately
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 
@@ -19,5 +22,6 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";
 git --version
 bash --version
 bash -c "pwsh --version"
+7z --version
 
 Write-Host "Base Packages installed successfully"
